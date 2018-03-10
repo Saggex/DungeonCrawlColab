019a4eecadc3bf3546cb5db94750555d2c245e6f
Lorechi <Lorena_chiara@freenet.de>
2018-03-10 20:17:53 +0100

    Added basic animations and a basic Movement Controller

---
+++ DungeonCrawlProject/Assets/Scripts/Movements/Charaktermovement.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Rigidbody2D))]
+public class Charaktermovement : MonoBehaviour {
+
+    Animator animator;
+    Rigidbody2D rb;
+
+    void Start()
+    {
+        animator = GetComponentInChildren<Animator>();
+        rb = GetComponent<Rigidbody2D>();
+    }
+    
+    public void Move(Vector2 direction)
+    {
+        if (animator)
+        {
+            animator.SetBool("Walking", true);
+        }
+        rb.velocity = direction;
+    }
+
+    public void Idle()
+    {
+        if (animator)
+        {
+            animator.SetBool("Walking", false);
+        }
+
+        rb.velocity = new Vector2(0, 0);
+    }
+}
---
+++ DungeonCrawlProject/Assets/Scripts/Movements/PlayerController.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : Charaktermovement {
+
+    public float speed = 1;
+
+	// Use this for initialization
+	
+	// Update is called once per frame
+	void Update () {
+		if(Input.GetAxis("Horizontal")!=0 || Input.GetAxis("Vertical") != 0)
+        {
+            Move((new Vector2(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical")))*speed);
+        }
+        else
+        {
+            Idle();
+        }
+    }
+}
